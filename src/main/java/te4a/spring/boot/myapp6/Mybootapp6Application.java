ed7034f45cc79b9f028d930cf179949b60a40f8e
te200741 <te200741@gmail.com>
2023-06-01 12:49:48 +0900

    first commit

---
+++ src/main/java/te4a/spring/boot/myapp6/Mybootapp6Application.java
@@ -0,0 +1,13 @@
+package te4a.spring.boot.myapp6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybootapp6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mybootapp6Application.class, args);
+	}
+
+}
---
+++ src/test/java/te4a/spring/boot/mybootapp6/Mybootapp6ApplicationTests.java
@@ -0,0 +1,13 @@
+package te4a.spring.boot.mybootapp6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybootapp6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
