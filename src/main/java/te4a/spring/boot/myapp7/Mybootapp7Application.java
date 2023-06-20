e9fed92d473da9d235f56e886d425cb7064ed679
te200741 <te200741@gmail.com>
2023-06-20 12:28:54 +0900

    kadai7　コミット

--- src/main/java/te4a/spring/boot/myapp7/Mybootapp7Application.java
+++ src/main/java/te4a/spring/boot/myapp7/Mybootapp7Application.java
@@ -1,13 +1,13 @@
-package te4a.spring.boot.myapp6;
+package te4a.spring.boot.myapp7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Mybootapp6Application {
+public class Mybootapp7Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Mybootapp6Application.class, args);
+		SpringApplication.run(Mybootapp7Application.class, args);
 	}
 
 }
