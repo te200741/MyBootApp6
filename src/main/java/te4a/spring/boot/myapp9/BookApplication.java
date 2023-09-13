6ca492bc0d3f5bf7dc4f5228fe13807bce3b60c3
te200741 <te200741@gmail.com>
2023-09-13 11:04:18 +0900

    kadai9 途中

---
+++ src/main/java/te4a/spring/boot/myapp9/BookApplication.java
@@ -0,0 +1,13 @@
+package te4a.spring.boot.myapp9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookApplication.class, args);
+	}
+
+}
